17258e03979178af0ae2be23fafead14f4b60c62
Asus <Asus@DESKTOP-CCER06C>
2017-07-10 20:43:42 +0300

    fix

--- ttt_test/Program.cs
+++ ttt_test/Program.cs
@@ -12,7 +12,7 @@ namespace ttt_test
         static void Main(string[] args)
         {
             var board = new CTTBoard();
-            board.ApplyStep(CTTBoard.Player.PlayerY, 0, 1);
+          board.ApplyStep(CTTBoard.Player.PlayerY, 0, 1);
             board.ApplyStep(CTTBoard.Player.PlayerY, 1, 2);
             board.ApplyStep(CTTBoard.Player.PlayerX, 0, 2);
             board.ApplyStep(CTTBoard.Player.PlayerX, 2, 2);
